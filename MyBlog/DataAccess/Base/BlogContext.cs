4898db4f0b60f2622868ed7d119b163e84bd1e71
264player <2309707428@qq.com>
2023-09-01 20:46:16 +0800

    增加了首页，导航栏，浏览所有博客，创建博客

---
+++ MyBlog/DataAccess/Base/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using MyBlog.Models;
+
+namespace MyBlog.DataAccess.Base
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) { }
+        public DbSet<ABlog> Blogs { get; set; }
+        public DbSet<ABContent> Contents { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ABlog>()
+                .HasKey(b => b.BId)
+                .HasName("PrimaryKey_BId");
+            modelBuilder.Entity<ABContent>()
+                .HasKey(b => b.BId)
+                .HasName("PrimatyKey_BId");
+        }
+    }
+}
---
+++ MyBlog/Models/Blogs.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBlog.Models
+{
+    public class Blogs
+    {
+        /// <summary>
+        /// 博客数组
+        /// </summary>
+        public List<ABlog> BlogList { get; set; }
+    }
+}
