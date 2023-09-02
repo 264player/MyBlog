3fc63495e94e56eed791d723d465de0923d99be5
264player <2309707428@qq.com>
2023-09-02 22:21:56 +0800

    增加了用户登录和用户注册的页面，并初步设计好了用户信息数据库

---
+++ MyBlog/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Creat()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
